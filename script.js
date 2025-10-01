f8f637b22513f01384c84467e65f7cb8a4d35ca2
chrisdemee <526cdemetri@frhsd.com>
2025-10-01 09:09:13 -0400

    array

---
+++ script.js
@@ -0,0 +1,12 @@
+const underground = [
+    "che",
+    "osamason",
+    "1oneam",
+    "xaviersobased",
+    "prettifun",
+    "autumn",
+    "boolymon",
+    "summrs",
+    "nettspend",
+    "yuke",
+]
